fc21c9e64b9668824966d67876c4191446bb7d20
lachieh <lachlan@heywood.net.au>
2015-02-20 16:37:43 +0000

    Merge PHP and HTML

--- index.php
+++ index.php
@@ -1,65 +1,72 @@
-<html>
-<head>
-<style type="text/css">
-	.full-details {
-		position: absolute;
-		background: rgba(0,0,0,0.1);
-		height: 500px;
-		overflow-y: scroll;
-		width: auto;
-		right: 0;
-		padding: 10px;
-		color: rgb(0,0,0);
-	}
-
-	.more{
-		position: absolute;
-		top: 20px;
-		right: 20px;
-	}
-
-	.button-more {
-		border: 1px solid rgba(220,150,50,1);
-		border-radius: 5px;
-		padding: 10px;
-		background: rgb(223, 117, 20);
-		color: rgb(255,255,255);
-	}
-
-	ul {list-style: none;}
-	ul.more li ul li {display: none;}
-	ul.more li:hover ul li {display: block;}
-</style>
-</head>
-<body>
-
-
 <?php
-require 'vendor/autoload.php';
-
-$api = new SpotifyWebAPI\SpotifyWebAPI();
-$track = $api->getTrack('3OtlhVRkLHpX1rUadM6QpQ');
-
-echo '<b>Track: </b>' . $track->name . '<br />';
-echo '<b>Artist: </b>' . $track->artists[0]->name . '<br>';
-echo '<b>' . ucfirst($track->album->album_type) . ' Name: </b>' . $track->album->name . '<br>';
-echo '<img src="' . $track->album->images[0]->url .'" />';
-
+	require 'vendor/autoload.php';
+	$api = new SpotifyWebAPI\SpotifyWebAPI();
+	$track = $api->getTrack('1aUwYQlraYDS9VjlLquvTT');
 ?>
 
-<ul class="debug more">
-	<li class="button-more">Debug
-		<ul>
-			<li class="full-details">
-				<?php
-					echo '<pre>';
-					print_r($track);
-					echo '</pre>';
-				?>
-			</li>
-		</ul>
-	</li>
-</ul>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1.0">
+	<title>index</title>
+	<link href="http://fonts.googleapis.com/css?family=Ubuntu+Condensed:400,400" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/standardize.css">
+	<link rel="stylesheet" href="css/index-grid.css">
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="css/debug.css">
+</head>
+<body class="body page-index clearfix">	
+	<nav class="nav-main clearfix">
+		<div class="container clearfix">
+			<div class="logo clearfix">
+				<div class="logo-image"></div>
+				<span class="logo-text">SPOTIFART</span>
+			</div>
+			<div class="menu"></div>
+		</div>
+	</nav>
+	<div class="container content clearfix">
+		<img class="image" title="<?php echo $track->name ?> by <?php echo $track->artists[0]->name ?> from the <?php echo $track->album->album_type?> <?php echo $track->album->name ?>" src="<?php echo $track->album->images[0]->url ?>">
+		<div class="meta clearfix">
+			<div class="infobox clearfix">
+				<div class="albuminfo clearfix">
+				<span class="album"><?php echo $track->album->name ?></span>
+				<span class="artist"><?php echo $track->artists[0]->name ?></span>
+				</div>
+				<button class="launch">OPEN IN SPOTIFY</button>
+				<div class="social">
+					<div class="width25 facebook clearfix">
+						<div class="icon-facebook"></div>
+					</div>
+					<div class="width25 google clearfix">
+						<div class="icon-google"></div>
+					</div>
+					<div class="width25 twitter clearfix">
+						<div class="icon-twitter"></div>
+					</div>
+					<div class="width25 imgur clearfix">
+						<div class="icon-imgur"></div>
+					</div>
+				</div>
+			</div>
+			<div class="player"></div>
+		</div>
+	</div>
+
+	<ul class="debug more">
+		<li class="button-more">Debug
+			<ul>
+				<li class="full-details">
+					<?php
+						echo '<pre>';
+						print_r($track);
+						echo '</pre>';
+					?>
+				</li>
+			</ul>
+		</li>
+	</ul>
 
 
 </body>
